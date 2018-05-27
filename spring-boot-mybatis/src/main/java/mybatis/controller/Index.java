f1b4c5425e2e373b417444dbf5655c373e6850cb
F1yBear <18717346409@qq.com>
2018-05-27 13:10:02 +0800

    新增跳转到首页swagger-ui的路由

---
+++ spring-boot-mybatis/src/main/java/mybatis/controller/Index.java
@@ -0,0 +1,14 @@
+package mybatis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+
+
+    @RequestMapping(value = "")
+    public  String index(){
+        return "/swagger-ui.html";
+    }
+}
