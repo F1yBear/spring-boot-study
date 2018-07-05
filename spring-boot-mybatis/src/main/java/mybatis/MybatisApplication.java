bbee166149c6bcc231760303a54b3ab67577900f
F1yBear <18717346409@qq.com>
2018-07-06 00:11:23 +0800

    spring-boot 整合 druid mybatis swagger

--- spring-boot-mybatis/src/main/java/mybatis/MybatisApplication.java
+++ spring-boot-mybatis/src/main/java/mybatis/MybatisApplication.java
@@ -3,6 +3,9 @@ package mybatis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class MybatisApplication {
 	public static void main(String[] args) {
--- spring-boot-mybatis/src/main/java/mybatis/controller/Index.java
+++ spring-boot-mybatis/src/main/java/mybatis/controller/Index.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Index {
 
 
-    @RequestMapping(value = "")
+  @RequestMapping("")
     public  String index(){
         return "/swagger-ui.html";
     }
